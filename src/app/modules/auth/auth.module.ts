415b0c2d90786d497c6befcfc2e5265c52087ec8
Kelwin <kelwin-hike@hotmail.com>
2019-01-12 21:30:49 -0200

    Validação do formulario de login OK

--- src/app/modules/auth/auth.module.ts
+++ src/app/modules/auth/auth.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms'
 
 import { NbSidebarModule, NbLayoutModule, NbSidebarService, NbInputModule, NbCardModule, NbButtonModule, NbCheckboxModule } from '@nebular/theme';
 
@@ -9,6 +10,7 @@ import { AuthRoutingModule } from './auth-routing.module';
 
 @NgModule({
   imports: [
+    ReactiveFormsModule,
     AuthRoutingModule,
     NbLayoutModule,
     NbSidebarModule,
--- src/app/modules/auth/components/login/login.component.ts
+++ src/app/modules/auth/components/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+
+  public form: FormGroup = new FormGroup({
+    'email': new FormControl(null, [Validators.email, Validators.required]),
+    'password': new FormControl(null, [Validators.minLength(5), Validators.required])
+  })
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  public login(): void {
+    console.log(this.form)
   }
 
 }
