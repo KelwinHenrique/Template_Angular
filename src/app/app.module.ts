493215f5b9fdea1da054034f3271be9a44c8bf19
Kelwin <kelwin-hike@hotmail.com>
2019-01-12 16:25:10 -0200

    Componente login criado e com rota

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { NbThemeModule } from '@nebular/theme';
 
+import { RouterModule } from '@angular/router';
+
 import { AppComponent } from './app.component';
 
 import { MainModule } from '../app/modules/main/main.module';
+import { AuthModule } from '../app/modules/auth/auth.module';
+
+import { LoginComponent } from '../app/modules/auth/components/login/login.component';
 
 @NgModule({
   declarations: [
@@ -13,7 +18,14 @@ import { MainModule } from '../app/modules/main/main.module';
   imports: [
     BrowserModule,
     NbThemeModule.forRoot(),
-    MainModule
+    RouterModule.forRoot(
+      [
+        { path: "login", component: LoginComponent},
+        { path: "main", component: AppComponent},
+      ]
+    ),
+    MainModule,
+    AuthModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/modules/main/main.module.ts
+++ src/app/modules/main/main.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { RouterModule } from '@angular/router';
 /**
  * Componnets Nebular
  */
-import { RouterModule } from '@angular/router';
 import { NbSidebarModule, NbLayoutModule, NbSidebarService } from '@nebular/theme';
 
 
